ab28bf9c3093027e67da902fd2119124e53e5015
Nicholas Sharp <naas8905@gmail.com>
2020-08-08 16:40:15 -0700

    Adding the user table, and create new movie

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_bcrypt import Bcrypt
 import io
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://owbwygdpwxsdpz:7ddd2951681719c841f7f77584f90f996a9e10a5e517b264fe6ade8e70753835@ec2-54-165-36-134.compute-1.amazonaws.com:5432/deloh8icstggnu"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -20,9 +20,8 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     year = db.Column(db.Integer(), nullable=False)
-    # Example: PG-13
     rated = db.Column(db.String(20), nullable=False)
-    released_on = db.Column(db.String(20), nullable=False)
+    released_on = db.Column(db.Date(), nullable=False)
     genre = db.Column(db.String(50), nullable=False)
     director = db.Column(db.String(50), nullable=False)
     plot = db.Column(db.String(4000), nullable=False)
@@ -47,10 +46,26 @@ class MovieSchema(ma.Schema):
 movie_schema = MovieSchema()
 movies_schema = MovieSchema(many=True)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.DateField(), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "password")
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
 @app.route("/movie/add", methods=["POST"])
 def movie_add():
-    # if request.content_type != "application.json":
-    #     return jsonify("Error")
+    if request.content_type != "application.json":
+        return jsonify("Error")
 
     post_data = request.get_json()
     title = post_data.get("title")
@@ -61,12 +76,60 @@ def movie_add():
     director = post_data.get("director")
     plot = post_data.get("plot")
 
+    new_movie = Movie(title, year, rated, released_on, genre, director, plot)
+    db.session.add(new_movie)
+    db.session.commit()
+
     return jsonify("Movie added")
     
 @app.route("/movies/get", methods=["GET"])
 def get_movies_data():
     movies_data = db.session.query(Movie).all()
     return jsonify(movies_schema.dump(movies_data))
+
+@app.route("/user/add", methods=["POST"])
+def create_user():
+    if request.content_type != "application/json":
+        return jsonify("Error")
+
+    post_data = request.get_json()
+    username = post_data.get("username")
+    password = post_data.get("password")
+
+    username_check = db.session.query(User.username).filter(User.username == username).first()
+    if username_check is not None:
+        return jsonify("Username taken")
+
+    hashed_password = bcrypt.generate_password_hash(password).decode("utf8")
+
+    record = User(username, hashed_password)
+    db.session.add(record)
+    db.session.commit()
+
+    return jsonify("User created")
+
+@app.route("/user/login", methods=["POST"])
+def verify_user():
+    if request.content_type != "application/json":
+        return jsonify("Error, data must be sent as json")
+
+    post_data = request.get_json()
+    username = post_data.get("username")
+    password = post_data.get("password")
+
+    stored_password = db.session.query(User.password).filter(User.username == username).first()
+    print(stored_password)
+    print(password)
+
+    if stored_password is None:
+        return jsonify("User not Verified")
+
+    valid_password_check = bcrypt.check_password_hash(stored_password[0], password)
+
+    if valid_password_check == False:
+        return jsonify("User not Verified")
+
+    return jsonify("User verified")
 
 if __name__ == "__main__":
     app.run(debug=True)
