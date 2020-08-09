7044ac596b9b6fecd5d2a7a2025d010bb72caead
Nicholas Sharp <naas8905@gmail.com>
2020-08-08 17:53:58 -0700

    Changing URI

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_bcrypt import Bcrypt
 import io
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://cguzrfeviaywsh:1dfa744f2cebe2db555daa9bb5ef450d7baa6efd37b17b6df0124531f9b845eb@ec2-107-22-7-9.compute-1.amazonaws.com:5432/d6p771rjqjo6u4"
+app.config["SQLALCHEMY_DATABASE_URI"] = ""
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
